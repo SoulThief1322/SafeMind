6e775cc0d14070e16f653a0db5a3a85c9dd99c71
SoulThief1322 <alenko2007@gmail.com>
2025-11-20 12:25:32 +0200

    First design of home page and Documentation

---
+++ SafeMind/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SafeMind.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
