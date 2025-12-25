ee22980038fd08d94af94781ec5ccf12baf26570
SoulThief1322 <alenko2007@gmail.com>
2025-12-25 19:13:26 +0200

    Initial book page design without functionality

---
+++ SafeMind/Controllers/ArticlesController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SafeMind.Models;
+
+namespace SafeMind.Controllers;
+
+public class ArticlesController : Controller
+{
+    private readonly ILogger<ArticlesController> _logger;
+
+    public ArticlesController(ILogger<ArticlesController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public async Task<IActionResult> Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- SafeMind/obj/Debug/net9.0/SafeMind.AssemblyInfo.cs
+++ SafeMind/obj/Debug/net9.0/SafeMind.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c939591aa33385f99e072d6a13f1f9ff5296b2c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63c6563de4a8ee9091b92d16113233c85879a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
