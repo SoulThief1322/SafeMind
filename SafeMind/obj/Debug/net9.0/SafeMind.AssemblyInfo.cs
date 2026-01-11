3ce9c86559d4fcb2509b78359e225890218098b1
SoulThief1322 <alenko2007@gmail.com>
2026-01-11 18:24:01 +0200

    Initial cshtml of my diary page

--- SafeMind/obj/Debug/net9.0/SafeMind.AssemblyInfo.cs
+++ SafeMind/obj/Debug/net9.0/SafeMind.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db276feb0ef2888958b6455df58bacea5b48a471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9ec4a26b6df54889090c90ec8f849faf54a349")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeMind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
