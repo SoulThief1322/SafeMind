126d3320889934eb68c0acccf22c66cb1e3e2fb7
SoulThief1322 <alenko2007@gmail.com>
2026-03-07 13:32:58 +0200

    admin panel added and contact us page

--- SafeMind/Program.cs
+++ SafeMind/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<SafeMind.Services.DiaryService>();
 builder.Services.AddScoped<SafeMind.Services.ArticleService>();
 builder.Services.AddScoped<SafeMind.Services.ChatService>();
 builder.Services.AddScoped<SafeMind.Services.GoalService>();
+builder.Services.AddScoped<SafeMind.Services.AdminService>();
 builder.Services.AddHostedService<SafeMind.Services.SessionCleanupService>();
 builder.Services.ConfigureApplicationCookie(options =>
 {
