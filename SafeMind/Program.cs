6a844e33433f2619cabc79308cf276b9d22b75dd
SoulThief1322 <alenko2007@gmail.com>
2026-04-12 22:30:23 +0300

    Add Google authentication callback path configuration

--- SafeMind/Program.cs
+++ SafeMind/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddAuthentication()
     {
         options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
         options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+        options.CallbackPath = "/auth/google/callback";
         options.Events.OnRemoteFailure = context =>
         {
             context.Response.Redirect("/");
